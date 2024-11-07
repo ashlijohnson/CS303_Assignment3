f79a41b8ef62db6db3982b161b69ef620cdf72c8
ashlijohnson <143415435+ashlijohnson@users.noreply.github.com>
2024-11-07 16:49:38 -0600

    Create queue.h

---
+++ Question_3/queue.h
@@ -0,0 +1,30 @@
+// Queue.h
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <typename T>
+class Queue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        Node(T val) : data(val), next(nullptr) {}
+    };
+
+    Node* frontNode;
+    Node* rearNode;
+    int count;
+
+public:
+    Queue();
+    ~Queue();
+    void push(const T& value);
+    void pop();
+    T front() const;
+    int size() const;
+    bool empty() const;
+    void move_to_rear();
+    void display();
+    void sort();
+};
